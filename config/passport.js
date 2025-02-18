868b0484b92ccd8a65ad4a1c353acff3fba43dca
Adithyan101 <adithyansaritha11@gmail.com>
2025-02-18 19:19:01 +0530

    Updated callback url google auth

--- config/passport.js
+++ config/passport.js
@@ -7,7 +7,7 @@ const env = require("dotenv").config()
 passport.use(new GoogleStrategy({
     clientID:process.env.GOOGLE_CLIENT_ID,
     clientSecret:process.env.GOOGLE_CLIENT_SECRET,
-     callbackURL:'/http://gamerr.store/auth/google/callback'
+     callbackURL:'/https://gamerr.store/auth/google/callback'
 
 },
 
